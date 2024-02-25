ae5583470d97da99c99049d36def4df291e4134e
NaraHiromichi <narahiromichi102938@gmail.com>
2024-02-25 14:58:47 +0630

    fixed eslint quote

--- app/components/account/Quote.tsx
+++ app/components/account/Quote.tsx
@@ -4,8 +4,8 @@ const Quote = () => {
   return (
     <div className="w-[90vw] mx-auto mt-4 text-primary text-base font-bold">
       <div className="w-full min-h-28 max-h-40 p-2 flex text-center items-center rounded-md bg-main">
-        "Your self-worth is determined by you. You don't have to depend on
-        someone telling you who you are."
+        &quot;Your self-worth is determined by you. You don't have to depend on
+        someone telling you who you are.&quot;
       </div>
       <span className="w-full mt-3 pr-1 flex justify-end items-center underline">
         Beyoncé
