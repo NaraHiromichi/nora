0606a529c0a4c4fcf9c40e00818fa22fbe005622
NaraHiromichi <narahiromichi102938@gmail.com>
2024-03-04 20:49:09 +0630

    03: deployment-fail remove unnecessary useSearchParams

--- app/auth/new-verification/page.tsx
+++ app/auth/new-verification/page.tsx
@@ -1,9 +1,8 @@
 "use cilent";
-import { newVerification } from "@/actions/new-verification";
+
 import NewVerification from "@/app/components/NewVerification";
-import { BackgroundGradientAnimation } from "@/app/components/ui/background-gradient-animation";
-import { useSearchParams } from "next/navigation";
-import React, { useCallback, useEffect, useState } from "react";
+
+import React from "react";
 
 export default function Page() {
   return <NewVerification />;
