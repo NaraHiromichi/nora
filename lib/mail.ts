6ad8634dbc7ea8227988ff2bea2acebc4ad311a9
NaraHiromichi <95912189+NaraHiromichi@users.noreply.github.com>
2024-03-05 16:10:07 +0630

    Update mail.ts added domain

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const EmailFrom = "onboarding@resend.dev";
+const EmailFrom = "mail@htet-wanna-linn.com";
 const resend = new Resend(process.env.RESEND_API_KEY);
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 
